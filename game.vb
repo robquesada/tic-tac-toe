b66eb78f9d051faf1ab47f2bfc9decbaa17c6d0b
robquesada <xroquear@gmail.com>
2014-09-17 16:37:28 -0600

    Game.rb added

---
+++ game.vb
@@ -0,0 +1,19 @@
+class Game
+
+  tictactoe = Matrix.build(3, 3)
+
+  def tic_tac_toe = [][]
+ 
+  def play_with_user
+  
+  end
+
+  def play_with_computer
+  
+  end
+
+  def player_entry(entry)
+    
+  end
+
+end
