417dbc673ce8f368f6b0ac771903e21cea49c654
robquesada <xroquear@gmail.com>
2014-09-19 09:43:30 -0600

    Updated branch

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH << '.'
 require 'game'
-include Game
+
 public
 
 def initial_menu
@@ -13,17 +13,57 @@ def initial_menu
   puts "********************************"
 
   option = gets.to_i
+  self.initialize_game
   self.start_game(option)
+end
+
+def initialize_game
+  @game = Core::Game.new
 end
 
 def start_game(option)
   if option == 1
-    Game.play_versus_player
+    @game.play_versus_player
   elsif option == 2
-    Game.play_versus_computer
+    @game.play_versus_computer
   else
     puts "Press 1 or 2"
   end
+  self.print_tictactoe
+  self.insert_entry_menu
+  self.print_tictactoe
+end
+
+def print_tictactoe
+  i = 0
+  @game.tictactoe.to_a.each do |row|
+   puts " #{row[0]}    #{row[1]} | #{row[2]} | #{row[3]}"
+   puts " " if i == 0
+   puts "    --------------" if i > 0 && i < 3 
+   i += 1
+  end
+end
+
+def insert_entry_menu
+  puts "*********************************************"
+  puts "Insert the number of the column of your entry"
+  puts "1, 2 or 3"
+  puts "*********************************************"
+
+  column = gets.to_i
+
+  puts "*********************************************"
+  puts "Now, insert the letter of your row"
+  puts "A, B or C"
+  puts "*********************************************"
+
+  row = gets.to_i
+  row = 1 if row == "A"
+  row = 2 if row == "B"
+  row = 3 if row == "C"
+
+  #@game.player_entry(row, column)
 end
 
 self.initial_menu
+
